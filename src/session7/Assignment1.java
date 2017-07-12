afb0f2b5507a003241c3bd3ab4038e902078db59
Sudhir.Nayak <Sudhir.Nayak@WLW7-SUDHIRN.INDECOMM.LOCAL>
2017-07-12 18:23:06 +0530

    Session 7 Assignment1

---
+++ src/session7/Assignment1.java
@@ -0,0 +1,20 @@
+package session7;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		//Declaring Integer Variables
+	    int intNumber1 = 49;
+	    int intNumber2 = 50;
+	    //displaying Integer values
+	    System.out.println("Before converting Integer to String "+(intNumber1 + intNumber2));
+	    
+	    //Converting Integer to String
+	    String number1AsString = Integer.toString(intNumber1);
+	    String number2AsString = Integer.toString(intNumber2);
+	    System.out.println("After converting Integer to String "+(number1AsString+number2AsString));;
+	    
+
+	}
+
+}
